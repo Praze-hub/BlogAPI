a826037a63039c474f3ec6912c329df8012dd401
God'spraise Okechukwu <69146860+Praze-hub@users.noreply.github.com>
2022-06-04 14:18:42 +0100

    Add files via upload

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -62,6 +62,8 @@ REST_FRAMEWORK ={
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -150,6 +152,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = 'static/'
 
